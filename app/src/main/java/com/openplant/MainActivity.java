fd2113efea7043b9d88b642e1cd2ff9606b7c5d7
Hugo Jourdain <hugo.jourdain@viacesi.fr>
2015-05-22 17:29:18 +0200

    Code de tappage du service web integré dans le MainActivity

--- app/src/main/java/com/openplant/MainActivity.java
+++ app/src/main/java/com/openplant/MainActivity.java
@@ -3,9 +3,12 @@ package com.openplant;
 import android.app.Activity;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -17,6 +20,17 @@ import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpResponseException;
+import org.ksoap2.transport.HttpTransportSE;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends Activity
@@ -45,6 +59,9 @@ public class MainActivity extends Activity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        CouchClientTask task = new CouchClientTask();
+        task.execute();
     }
 
     @Override
@@ -146,4 +163,53 @@ public class MainActivity extends Activity
         }
     }
 
+    private class CouchClientTask extends AsyncTask<Void, Void, List<String>> {
+
+        private static final String action = "GetFirstData";
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+            SoapObject request = new SoapObject("http://tempuri.org/", action);
+            envelope.bodyOut = request;
+            HttpTransportSE transport = new HttpTransportSE("http://10.0.2.2:8734/MongoService/Service/?wsdl");
+
+            try {
+                transport.call("http://tempuri.org/IService/" + action, envelope);
+            } catch (HttpResponseException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            }
+
+            SoapObject results = (SoapObject)envelope.bodyIn;
+
+            List<String> data = new ArrayList<String>();
+
+            data.add(results.getAttributeAsString(0));
+
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(List<String> result) {
+            StringBuilder sb = new StringBuilder();
+
+            for(String s: result)
+                sb.append(s + "\n");
+
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Results")
+                    .setMessage(sb.toString())
+                    .setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // null
+                        }
+                    })
+                    .show();
+        }
+    }
 }
